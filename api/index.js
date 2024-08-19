cf8d8ae76338cef8bd9eb210f2f2d0859a5ee01c
Mariia <mcherkashina77@gmail.com>
2024-08-19 18:09:49 +0200

    add get endpoint

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const projectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  skills: { type: String, required: true },
+  link: { type: String, required: true },
+  role: { type: String, required: true },
+  workedOn: { type: String, required: true },
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+app.get("/api/projects", async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.json(projects);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = app;
